3da17c6e4aa33e27ef1b20dd16b96c004b8151bb
Nathan <nathan.jbpoole@hotmail.co.uk>
2021-07-20 14:23:26 +0100

    added 'st', 'nd' or 'th' after team position

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const randomTeam = team[Math.floor(Math.random() * team.length)];
 
 console.log(randomTeam)
 
+let position = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th',
+ '14th', '15th', '16th', '17th', '18th', '19th', '20th']
 
-let randomPosition = Math.floor(Math.random() * 21)
+let randomPosition = position[Math.floor(Math.random() * position.length)];
 
 console.log(randomPosition)
 
