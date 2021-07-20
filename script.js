c68d6ce5414d21dec86664bf1b965cf877d1ffc8
Nathan <nathan.jbpoole@hotmail.co.uk>
2021-07-20 13:46:41 +0100

    added random position and random goals

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let randomTeam = ['Arsenal', 'Aston Villa', 'Brentford', 'Brighton', 'Burnley', 'Chelsea', 'Crystal Palace', 
+let team = ['Arsenal', 'Aston Villa', 'Brentford', 'Brighton', 'Burnley', 'Chelsea', 'Crystal Palace', 
 'Everton', 'Leeds', 'Leicester', 'Liverpool', 'Manchester City', 'Manchester United', 'Newcastle', 'Norwich', 
 'Southampton', 'Tottenham', 'Watford', 'West Ham', 'Wolves'
-]
-const randomElement = randomTeam[Math.floor(Math.random() * randomTeam.length)]
-console.log(randomElement)
+];
+const randomTeam = team[Math.floor(Math.random() * team.length)];
+
+console.log(randomTeam)
+
+let randomPosition = Math.floor(Math.random() * 21)
+
+console.log(randomPosition)
+
+let randomGoals = Math.floor(Math.random() * 150);
+
+console.log(randomGoals)
