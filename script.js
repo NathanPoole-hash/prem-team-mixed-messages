a8e0fca3597b01eccdaa2ad6d94acd948e46b191
Nathan <nathan.jbpoole@hotmail.co.uk>
2021-07-20 11:40:11 +0100

    Added array of prem teams

---
+++ script.js
@@ -0,0 +1,5 @@
+let randomTeam = ['Arsenal', 'Aston Villa', 'Brentford', 'Brighton', 'Burnley', 'Chelsea', 'Crystal Palace', 
+'Everton', 'Leeds', 'Leicester', 'Liverpool', 'Manchester City', 'Manchester United', 'Newcastle', 'Norwich', 
+'Southampton', 'Tottenham', 'Watford', 'West Ham', 'Wolves'
+]
+randomTeam [0]
