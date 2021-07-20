10893fe7546a939c228d8d219db6609d0a546c42
Nathan <nathan.jbpoole@hotmail.co.uk>
2021-07-20 12:02:23 +0100

    tested array with node and works fine

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ let randomTeam = ['Arsenal', 'Aston Villa', 'Brentford', 'Brighton', 'Burnley', 
 'Everton', 'Leeds', 'Leicester', 'Liverpool', 'Manchester City', 'Manchester United', 'Newcastle', 'Norwich', 
 'Southampton', 'Tottenham', 'Watford', 'West Ham', 'Wolves'
 ]
-randomTeam [0]
+console.log(randomTeam[0])
+
